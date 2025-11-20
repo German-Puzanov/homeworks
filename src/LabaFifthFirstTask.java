2f2600436e66f459b97491b2496ab76e10f617c9
german <puzanovgerman2007@gmail.com>
2025-11-20 17:12:52 +0300

    add rectangle triangle check

--- src/LabaFifthFirstTask.java
+++ src/LabaFifthFirstTask.java
@@ -47,14 +47,23 @@ public class LabaFifthFirstTask {
         }
 
         System.out.println(x1 + " " + y1 + " " + x2 + " " + y2 + " " + x3 + " " + y3);
-        System.out.printf("%.2f", area(x1, y1, x2, y2, x3, y3));
+        if (area(x1, y1, x2, y2, x3, y3) == -1.00) {
+            System.out.println("Треугольник не прямоугольный не могу посчитать");
+        } else {
+            System.out.printf("%.2f", area(x1, y1, x2, y2, x3, y3));
+        }
 
     }
 
     static double area(int x1, int y1, int x2, int y2, int x3, int y3) {
-        double lengthAC = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        double lengthCE = Math.sqrt(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2));
-        return 0.5 * lengthAC * lengthCE;
+        if (((x1 - x2) * (x3 - x2) + (y1 - y2) * (y3 - y2)) == 0 ||
+                ((x2 - x1) * (x3 - x2) + (y2 - y1) * (y3 - y2)) == 0 ||
+                ((x1 - x3) * (x2 - x3) + (y1 - y3) * (y2 - y3)) == 0) {
+            double lengthAC = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+            double lengthCE = Math.sqrt(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2));
+            return 0.5 * lengthAC * lengthCE;
+        } else {
+            return -1;
+        }
     }
-
 }
