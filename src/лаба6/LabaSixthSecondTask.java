6122725864c769efbc44433115306d2fb546b922
German-Puzanov <102613225+German-Puzanov@users.noreply.github.com>
2025-11-23 20:26:33 +0300

    version 1.0

--- src/лаба6/LabaSixthSecondTask.java
+++ src/лаба6/LabaSixthSecondTask.java
@@ -17,7 +17,7 @@ public class LabaSixthSecondTask {
         String[] numArrStr = intEntry.split(" ");
         int[] numArr = new int[numArrStr.length];
 
-        for (int i = 0; i < numArrStr.length; i++){
+        for (int i = 0; i < numArrStr.length; i++) {
             try {
                 int digit = Integer.parseInt(numArrStr[i]);
                 numArr[i] = digit;
@@ -28,8 +28,36 @@ public class LabaSixthSecondTask {
             }
 
         }
+        int resultLenght = 0;
         System.out.println(Arrays.toString(numArr));
+        for (int i = 0; i < strArr.length; i++) {
+            for (int j = 0; j < numArr.length; j++) {
+                if (strArr[i].length() == numArr[j]) {
+                    resultLenght += 1;
+                }
+            }
+        }
 
+        System.out.println(resultLenght);
+        String[] result = new String[resultLenght];
+        int index = 0;
+        for (int i = 0; i < strArr.length; i++) {
+            boolean rigthLenght = false;
+            for (int j = 0; j < numArr.length; j++) {
+                if (strArr[i].length() == numArr[j]) {
+                    rigthLenght = true;
+                    break;
+                }
+            }
+            if (rigthLenght) {
+                result[index] = strArr[i];
+                index += 1;
+            }
+        }
+        for (String k : result){
+            System.out.print(k + " ");
+        }
     }
+
 
 }
