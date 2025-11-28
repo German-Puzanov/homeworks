15a1e57f6dc77896261bf1fe32e0f1c108622969
german <puzanovgerman2007@gmail.com>
2025-11-28 11:24:13 +0300

    add negative entry error

--- src/Lab7/LabaSevenFirstTask.java
+++ src/Lab7/LabaSevenFirstTask.java
@@ -9,13 +9,17 @@ public class LabaSevenFirstTask {
         try {
             int rows = Integer.parseInt(scanner.nextLine());
             int columns = Integer.parseInt(scanner.nextLine());
+            if (rows <= 0 || columns <= 0){
+                System.out.println("Error");
+                return;
+            }
             int[][] arr = new int[rows][columns];
             boolean reverse = true;
             int columnPos = columns - 1;
             int rowPos = rows - 1;
 
             for (int i = 1; i <= (rows * columns); i++) {
-                arr[rowPos][columnPos] = i*N;
+                arr[rowPos][columnPos] = i + N - 1;
                 if (reverse) {
                     if (rowPos == 0) {
                         columnPos -= 1;
@@ -34,12 +38,12 @@ public class LabaSevenFirstTask {
             }
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < columns; j++) {
-                    System.out.print("  " + arr[i][j]);
+                    System.out.print("\t" + arr[i][j]);
                 }
                 System.out.println();
             }
-        } catch (NegativeArraySizeException | NumberFormatException e) {
-            System.out.println("error");
+        } catch (NumberFormatException e) {
+            System.out.println("Error");
             scanner.close();
         }
     }
