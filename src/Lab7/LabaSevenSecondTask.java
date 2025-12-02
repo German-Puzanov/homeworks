9c3f9610e4b08b9a7e9ce76c2d7d72921e0bf553
german <puzanovgerman2007@gamil.com>
2025-12-02 22:31:42 +0300

    bug fix logic fix

--- src/Lab7/LabaSevenSecondTask.java
+++ src/Lab7/LabaSevenSecondTask.java
@@ -1,6 +1,5 @@
 package Lab7;
 
-import java.util.Arrays;
 import java.util.Scanner;
 public class LabaSevenSecondTask {
     public static void main(String[] args) {
@@ -8,42 +7,73 @@ public class LabaSevenSecondTask {
         int number = 0;
         while (number < 2 || number % 2 != 0) {
             try {
-                number = Integer.parseInt(scanner.nextLine());
+                number = Integer.parseInt(scanner.nextLine().trim());
                 if (number < 2 || number % 2 != 0){
-                    System.out.println("Please, try again!");
+                    System.out.println("Please, try again! 1");
                     continue;
                 }
 
             } catch (NumberFormatException ex) {
-                System.out.println("Please, try again!");
+                System.out.println("Please, try again! 2");
                 continue;
             }
             String[][] strArr = new String[number][number];
-            while (true){
-                boolean valid = true;
-                for (int i = 0; i < number; i++){
-                    String[] strEntry = scanner.nextLine().split(" ");
+            int[][] matrix = new int[number][number];
+            boolean valid;
+            while (true) {
+                valid = true;
+                for (int i = 0; i < number; i++) {
+                    String[] strEntry = scanner.nextLine().trim().split(" ");
                     strArr[i] = strEntry;
-                    if (strEntry.length < number){
+                    if (strEntry.length < number) {
                         valid = false;
                         break;
                     }
                 }
-                int[][] matrix = new int[number][number];
-                if (valid) {
+                    if (!valid) {
+                        System.out.println("Please, try again! 3");
+                        continue;
+                    }
+
                     for (int row = 0; row < number; row++) {
                         for (int num = 0; num < number; num++) {
-                            matrix[row][num] = Integer.parseInt(strArr[row][num]);
+                            try {
+                                matrix[row][num] = Integer.parseInt(strArr[row][num]);
+                            } catch (NumberFormatException e) {
+                                valid = false;
+
+                            }
                         }
                     }
+
+                if (!valid){
+                    System.out.println("Please, try again! 3");
                 }
                 else{
-                    System.out.println("Please, try again!");
-                    continue;
+                    break;
                 }
-                System.out.println(Arrays.deepToString(matrix));
-                break;
+            }
+            int firstSum = 0;
+            int secondSum = 0;
+            int thirdSum = 0;
+            int fourthSum = 0;
+            int halfNum = number/2;
+            for (int i = 0; i < halfNum; i++){
+                for (int j = 0; j < halfNum; j++){
+                    firstSum += matrix[i][j];
+                    thirdSum += matrix[halfNum+i][j];
+                    }
+                }
+            for (int i = 0; i < halfNum; i++){
+                for (int j = 0; j < halfNum; j++){
+                    secondSum += matrix[i][j+halfNum];
+                    fourthSum += matrix[halfNum+i][j+halfNum];
+                }
+            }
+            System.out.println(firstSum);
+            System.out.println(secondSum);
+            System.out.println(thirdSum);
+            System.out.println(fourthSum);
             }
         }
     }
-}
