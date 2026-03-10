8b74ac6d14ab34b7596aa563f95a4b36f635ade9
German <puzanovgerman2007@gmail.com>
2026-03-10 17:43:32 +0300

    add output

--- src/LabOOP3FirstTask/Main.java
+++ src/LabOOP3FirstTask/Main.java
@@ -2,16 +2,26 @@ package LabOOP3FirstTask;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            var worker = new Worker("Ivan");
-            var student = new Student("Dima");
-            var pensioner = new Pensioner("Nikolai");
 
+        try {
+            var worker = new Worker("");
             System.out.println(worker);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            var student = new Student("Dima");
             System.out.println(student);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            var pensioner = new Pensioner("Nikolai");
             System.out.println(pensioner);
 
-        } catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
 
