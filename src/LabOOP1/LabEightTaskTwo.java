986494d592671703584d855f44f4668479c9b8e4
german <puzanovgerman2007@gmail.com>
2026-03-18 21:52:56 +0300

    ///

--- src/LabOOP1/LabEightTaskTwo.java
+++ src/LabOOP1/LabEightTaskTwo.java
@@ -7,33 +7,31 @@ public class LabEightTaskTwo {
         Scanner scanner = new Scanner(System.in);
 
         try {
-            System.out.print("Введите имя козы/козла (длина имени >= 3 символа): ");
-            String name = scanner.nextLine().trim();
-
-            int age;
-            System.out.print("Введите возраст козы/козла: ");
-            try {
-                age = Integer.parseInt(scanner.nextLine().trim());
-
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid age!");
-                return;
-            }
-            System.out.print("Введите коза или козел (male/female): ");
-            String gender = scanner.nextLine().trim();
-
-            Goat goat = new Goat(name, gender, age);
-            System.out.println(goat.getInfo());
-            while(!scanner.nextLine().trim().equalsIgnoreCase("q")){
-                goat.addOneYear();
-                System.out.println("Через год: \n" +  goat.getInfo());
-            }
-
+//            System.out.print("Введите имя козы/козла (длина имени >= 3 символа): ");
+//            String name = scanner.nextLine().trim();
+//
+//            int age;
+//            System.out.print("Введите возраст козы/козла: ");
+//            try {
+//                age = Integer.parseInt(scanner.nextLine().trim());
+//
+//            } catch (NumberFormatException e) {
+//                System.out.println("Invalid age!");
+//                return;
+//            }
+//            System.out.print("Введите коза или козел (male/female): ");
+//            String gender = scanner.nextLine().trim();
+//
+//            Goat goat = new Goat(name, gender, age);
+//            System.out.println(goat.getInfo());
+//            while(!scanner.nextLine().trim().equalsIgnoreCase("q")){
+//                goat.addOneYear();
+//                System.out.println("Через год: \n" +  goat.getInfo());
+//            }
+//            scanner.close();
+new Goat("HDKJhhh", null, -5);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
-
-
-
     }
 }
