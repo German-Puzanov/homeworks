4f0ef0856fbb0d1745dd58f731798ff2b2b2b4b4
German <puzanovgerman2007@gmail.com>
2026-04-13 19:46:34 +0300

    Working version

--- src/LabOOP4SecondTask/Calculator.java
+++ src/LabOOP4SecondTask/Calculator.java
@@ -3,38 +3,57 @@ package LabOOP4SecondTask;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Calculator {
     static String path = "resourses/text2.txt";
     static final String calculationPath = "resourses/calculation.txt";
-    static String[] temp;
+    static String temp;
     static final String[] digits = {"0", "1", "2", "3", "4", "5", "6", "7", "8"};
     private static String[] inputArray;
     private static String[] outputArray;
 
 
     private static void inputSetter() throws IOException {
-
+        int length = 0;
         if (!Files.exists(Paths.get(path))) {
             throw new FileNotFoundException("Wrong path selected!");
         }
 
-        try (FileReader reader = new FileReader(path);
-             Scanner scanner = new Scanner(reader)) {
-            temp = scanner.nextLine().trim().split("\n");
-            inputArray = temp;
+        try (FileReader reader = new FileReader(path); Scanner scanner = new Scanner(reader)) {
+            String line;
+            while (scanner.hasNextLine()) {
+                line = scanner.nextLine();
+                if (!line.isBlank()){
+                    length++;
+                }
+            }
+            if (length == 0) throw new Exception("Nothing to calculate!");
 
-        } catch (IOException e) {
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        try (FileReader reader = new FileReader(path); Scanner scanner = new Scanner(reader)) {
+            String line;
+            int index = 0;
+            String[] input = new String[length];
+            while (scanner.hasNextLine()) {
+                line = scanner.nextLine();
+                if (!line.isBlank()){
+                    input[index] = line;
+                    index++;
+                }
+            }
+            inputArray = input;
+
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static void calculateInputs() throws Exception {
+    public static void calculateInputs() {
 
-        if (inputArray == null) {
-            throw new Exception("Invalid value");
-        }
         boolean isCorrectNum;
         String[] splitNum;
         String[] output = new String[inputArray.length];
@@ -45,6 +64,7 @@ public class Calculator {
             isCorrectNum = false;
             splitNum = inputArray[i].split("");
             for (int j = 0; j < splitNum.length; j++) {
+                isCorrectNum = false;
                 for (String digit : digits) {
                     if (splitNum[j].equals(digit)) {
                         isCorrectNum = true;
@@ -52,22 +72,30 @@ public class Calculator {
                     }
                 }
                 if (!isCorrectNum) {
-                    output[i] = "Invalid";
+                    output[i] = "Invalid value (contains not only digits)";
                     break;
                 }
             }
             if (isCorrectNum) {
                 scale = findMaxScale(splitNum);
                 long addedNumber;
-                for (int j = splitNum.length - 1; j > 0; j--) {
-                    addedNumber = Integer.parseInt(splitNum[j]);
-                    resultNum += (int) (addedNumber * Math.pow(scale, i));
-                    if (resultNum > Integer.MAX_VALUE) {
-                        output[i] = "invalid (too big to convert to in Integer)";
+                int pow = 0;
+                boolean tooBigNumber = false;
+                for (int j = splitNum.length - 1; j >= 0; j--) {
+                    addedNumber = Integer.parseInt(splitNum[j]) * ((long) Math.pow(scale, pow));
+                    if (addedNumber + resultNum > Integer.MAX_VALUE) {
+                        tooBigNumber = true;
                         break;
                     }
+                    resultNum += addedNumber;
+                    pow++;
+                }
+                if (tooBigNumber){
+                    output[i] = "invalid value (too big to convert to Integer)";
+                    continue;
                 }
                 output[i] = String.format("%d", resultNum);
+                resultNum = 0;
             }
         }
         outputArray = output;
@@ -82,23 +110,21 @@ public class Calculator {
                 scale = num;
             }
         }
-        return scale + 1;
+        return (scale + 1);
     }
 
-    public static void calculatorReport() throws Exception {
+    public void calculatorReport() throws Exception {
         inputSetter();
         calculateInputs();
-        String[] calculationArray = new String[inputArray.length];
+        StringBuilder calculation = new StringBuilder();
 
         for (int i = 0; i < inputArray.length; i++) {
-            calculationArray[i] = String.format("%s = %s", inputArray[i], outputArray[i]);
+            calculation.append(String.format("%s = %s\n", inputArray[i], outputArray[i]));
         }
 
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(calculationPath))) {
-
-            out.writeObject(calculationArray);
+        try (FileWriter out = new FileWriter(calculationPath)) {
+            out.write(calculation.toString());
             out.flush();
-
         } catch (IOException ex) {
             System.out.print("Something gone wrong!");
         }
--- src/LabOOP4SecondTask/Main.java
+++ src/LabOOP4SecondTask/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         try{
             Calculator calc = new Calculator();
-
+            calc.calculatorReport();
         } catch (Exception e){
             System.out.println(e.getMessage());
         }
